5783d6a6426b2122ae45893ba435d4ac4521acd6
MichaelMalysh <41323541+MichaelMalysh@users.noreply.github.com>
2018-10-28 10:19:51 +0200

    Update Student.java

--- Lesson4home/src/com/gmail/mmalyshmmm/Student.java
+++ Lesson4home/src/com/gmail/mmalyshmmm/Student.java
@@ -4,7 +4,7 @@ public class Student extends Human {
 	private String faculty;
 	private String formOfStudy;
 
-	public Student(String name, String surName, String middleName, String age, boolean sex, String faculty,
+	public Student(String name, String surName, String middleName, int age, boolean sex, String faculty,
 			String formOfStudy) {
 		super(name, surName, middleName, age, sex);
 		this.faculty = faculty;
@@ -16,7 +16,7 @@ public class Student extends Human {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Student(String name, String middleName, String surName, String age, boolean sex) {
+	public Student(String name, String middleName, String surName, int age, boolean sex) {
 		super(name, middleName, surName, age, sex);
 		// TODO Auto-generated constructor stub
 	}
@@ -24,7 +24,8 @@ public class Student extends Human {
 	public String getFaculty() {
 		return faculty;
 	}
-@Override
+
+	@Override
 	public boolean equals(Object obj) {
 		if (obj == this) {
 			return true;
@@ -32,28 +33,31 @@ public class Student extends Human {
 		if (obj == null || obj.getClass() != this.getClass()) {
 			return false;
 		}
-		Student other  = (Student) obj;
-		 if ((this.getName() == null) ? (other.getName() != null) : !this.getName().equals(other.getName())) {
-		        return false;
-		    }
-		 if ((this.getMiddleName() == null) ? (other.getMiddleName() != null) : !this.getMiddleName().equals(other.getMiddleName())) {
-		        return false;
-		    }
-		 if ((this.getSurName() == null) ? (other.getSurName() != null) : !this.getSurName().equals(other.getSurName())) {
-		        return false;
-		    }
-		 if ((this.getFaculty() == null) ? (other.getFaculty() != null) : !this.getFaculty().equals(other.getFaculty())) {
-		        return false;
-		    }
-		 if ((this.getAge() == null) ? (other.getAge() != null) : !this.getAge().equals(other.getAge())) {
-		        return false;
-		    }
-		 if ((this.getClass() == null) ? (other.getClass() != null) : !this.getClass().equals(other.getClass())) {
-		        return false;
-		    }
+		Student other = (Student) obj;
+		if ((this.getName() == null) ? (other.getName() != null) : !this.getName().equals(other.getName())) {
+			return false;
+		}
+		if ((this.getMiddleName() == null) ? (other.getMiddleName() != null)
+				: !this.getMiddleName().equals(other.getMiddleName())) {
+			return false;
+		}
+		if ((this.getSurName() == null) ? (other.getSurName() != null)
+				: !this.getSurName().equals(other.getSurName())) {
+			return false;
+		}
+		if ((this.getFaculty() == null) ? (other.getFaculty() != null)
+				: !this.getFaculty().equals(other.getFaculty())) {
+			return false;
+		}
+		if (this.getAge() == 0 && other.getAge() == 0) {
+			return false;
+		}
+		if ((this.getClass() == null) ? (other.getClass() != null) : !this.getClass().equals(other.getClass())) {
+			return false;
+		}
 
-		    return true;
-		
+		return true;
+
 	}
 
 	public void setFaculty(String faculty) {
